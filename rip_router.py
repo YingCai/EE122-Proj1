3a493c0a20103812dc4377d25f3dad5faa0e5f4c
Ying Cai <ycai92@gmail.com>
2013-10-11 00:52:44 -0700

    i think it works now

--- rip_router.py
+++ rip_router.py
@@ -23,55 +23,100 @@ class RIPRouter (Entity):
         #state[via][dest] gives distance
 
     def handle_rx (self, packet, port):
-        if packet.__class__.__name__ == "DiscoveryPacket":
-        	if packet.is_link_up:
-        		self.distances[packet.src] = 1
-        		self.state[packet.src] = {packet.src:1}
-        		self.ports[packet.src] = port
-        		self.vias[packet.src] = packet.src
+        if isinstance(packet, DiscoveryPacket):
+            print self.name + " received a discovery packet from " + str(packet.src)
+            if packet.is_link_up:
+                self.state[packet.src] = {packet.src:1}
+                self.ports[packet.src] = port
+                self.update_vectors()
 
-        		self.update_vectors()
+            else:
+                del self.distances[packet.src]
+                del self.state[packet.src]
+                del self.ports[packet.src]
+                del self.vias[packet.src]
+                for destination in self.vias.copy():
+                    if self.vias[destination] == packet.src:
+                        del self.vias[destination]
+                        del self.distances[destination]
+                for neighbor in self.state:
+                    if packet.src in self.state[neighbor]:
+                        del self.state[neighbor][packet.src]
+                print "Distances: " + str(self.distances)
+                print "State: " + str(self.state)
+   
+                self.update_vectors(True, updateSource = packet.src)
 
-        	else:
-        		del self.distances[packet.src]
-        		del self.vias[packet.src]
-        		del self.state[packet.src]
-        		del self.ports[packet.src]
+            #print "New distances: " + str(self.distances)
 
-        		self.update_vectors()
+        elif isinstance(packet, RoutingUpdate):
+            print self.name + " received a routing update packet from " + str(packet.src) + "..."
+            #print "Routing packet contents: " + str(packet.paths)
+            if packet.src not in self.distances:
+                print "Dropping RoutingUpdate packet at " + self.name + ": Not connected to packet source"
+            else:
+                print "Processing routing update packet from " + str(packet.src)
+                self.state[packet.src] = packet.paths
+                for destination in self.state[packet.src].copy():
+                    self.state[packet.src][destination]+=1
+                self.state[packet.src][packet.src]=1 #distance vector might not include the source itself
+                if self.__repr__() in self.state:
+                    del self.state[self.name] #state doesn't need path to self
+                self.update_vectors()
 
-        		#updateMessage = RoutingUpdate()
-        		#RoutingUpdate.paths = distances
-        		#send(updateMessage, port, true)
-        elif packet.__class.__name__ == "RoutingUpdate":
-        	self.state[packet.src] = packet.paths
-        	self.state[packet.src][packet.src]=1 #distance vector might not include the source itself
-        	if self.name in state:
-				del self.state[self.name] #this entry is extraneous
-        	self.update_vectors()
+
+            #print "New distances: " + str(self.distances)
+
         else:
-        	send(packet, ports[vias[packet.dst]], False)
+            print self.name + " received a data packet from " + str(packet.src)
+            #print "Ports dictionary: " + str(self.ports) + "\n"
+            #print "Vias dictionary: " + str(self.vias) + "\n"
+            if packet.dst in self.distances:
+                self.send(packet, self.ports[self.vias[packet.dst]], False)
+            else:
+                print "Dropping packet at " + self.name + "! "
+                print str(packet.dst) + " seems unreachable from here."
 
-    def update_vectors (self):
-    	#state table has changed, update vias and distances
-    	changes_made = False
-    	for neighbor in self.state.keys():
-    		for destination in self.state[neighbor].keys():
-    			if self.state[neighbor][destination] < self.distances[destination] or \
-    			   (self.state[neighbor][destination] == self.distances[destination] and \
-    				self.ports[neighbor] < self.ports[self.vias[destination]]):
-    				self.distances[destination] = self.neighbor_vector[destination]
-    				self.vias[destination] = neighbor
-    				changes_made = True
+    def update_vectors (self, changes_made = False, updateSource = None):
+        #state table has changed, update vias and distances
+        #print "Updating vectors. State dict: " + str(self.state)
+        for destination in self.vias.copy():
+            if destination not in self.state[self.vias[destination]]:
+                del self.vias[destination]
+                del self.distances[destination]
+                changes_made = True
 
-    	if changes_made:
-    		for neighbor in ports:
-    			paths = self.distances.copy()
+        for neighbor in self.state:
+            for destination in self.state[neighbor]:
+                #print "State dictionary: " + str(self.state) + "\n"
+                #print "Vias dictionary: " + str(self.vias) + "\n"
+                #print "Ports dictionary: " + str(self.ports) + "\n"
+                #print "Distances dictionary: " + str(self.distances) + "\n"
 
-    			for destination in self.vias.keys():
-    				if self.vias[destination] == neighbor:
-    					del self.paths[destination]
+                if (destination not in self.distances) or \
+                    self.state[neighbor][destination] < self.distances[destination] or \
+                   (self.state[neighbor][destination] == self.distances[destination] and \
+                    self.ports[neighbor] < self.ports[self.vias[destination]]):
+                   #we have a new destination from a neighbor in state, or
+                   #we have a faster neighbor for a destination, or
+                   #we have a new neighbor for a destination with a lower port number
 
-    			updatePacket = RoutingUpdate()
-    			RoutingUpdate.paths = paths
-    			send(updatePacket, self.ports[neighbor], False)
+                    self.distances[destination] = self.state[neighbor][destination]
+                    self.vias[destination] = neighbor
+                    changes_made = True
+
+        if changes_made:
+            for neighbor in self.ports:
+                if neighbor == updateSource:
+                   continue
+                tailored_paths = self.distances.copy()
+                #print "tailored paths before tailoring: " + str(tailored_paths)
+
+                for destination in self.vias.keys(): #poison reverse
+                    if self.vias[destination] == neighbor:
+                        del tailored_paths[destination]
+
+                #print "tailored paths after tailoring: " + str(tailored_paths)
+                updatePacket = RoutingUpdate()
+                updatePacket.paths = tailored_paths
+                self.send(updatePacket, self.ports[neighbor], False)
